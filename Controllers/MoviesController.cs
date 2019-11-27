dd42127db81d35013dc696e55c8037c31c7c7bcc
atikartyas <58196244+atikartyas@users.noreply.github.com>
2019-11-27 16:19:34 +0700

    Initial commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using utproject.Models;
+
+namespace utproject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movie/random
+        public ActionResult random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="utproject.MvcApplication" Language="C#" %>
